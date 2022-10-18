fd5abd0534fbc1df9dc256ab40942bd03ef794e9
alejodevelop <72043595+alejodevelop@users.noreply.github.com>
2022-10-18 03:08:45 -0500

    Add files via upload

---
+++ OperacionesBasicasSQL.sql
@@ -0,0 +1,9 @@
+/*
+INSERT INTO persona(nombre, apellido, email, telefono) VALUES('karla', 'Lara', 'klara@gmail.com','55668811');
+*/
+/*
+UPDATE persona SET nombre = 'Juan', apellido = 'Perez' WHERE id_persona = 1;
+*/
+/*
+DELETE FROM persona WHERE id_persona = 4;
+*/
